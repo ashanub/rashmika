59775d338bf88522e64ea3796304211a592ebd6c
ashanub <ashanudayanga@gmail.com>
2022-11-05 21:50:04 -0400

    minot change

--- gridsome.config.js
+++ gridsome.config.js
@@ -1,39 +1,25 @@
-// This is where project configuration and plugin options are located.
-// Learn more: https://gridsome.org/docs/config
-
-// Changes here require a server restart.
-// To restart press CTRL + C in terminal and run `gridsome develop`
-
 module.exports = {
   siteName: 'Gridsome',
   transformers: {
     remark: {
       externalLinksTarget: '_blank',
       externalLinksRel: ['nofollow', 'noopener', 'noreferrer'],
-      anchorClassName: 'icon icon-link',
-      plugins: [
-        // ...global plugins
-      ]
+      anchorClassName: 'icon icon-link'
     }
   },
+
   plugins: [
     {
       use: '@gridsome/source-filesystem',
       options: {
         path: 'posts/**/*.md',
-        typeName: 'Post',
-        remark: {
-          plugins: [
-            // ...local plugins
-          ]
-        }
+        typeName: 'Post'
       }
     },
     {
       use: `gridsome-plugin-netlify-cms`,
       options: {
-        publicPath: `/admin`,
-        modulePath: `src/admin/index.js`
+        publicPath: `/admin`
       }
     },
   ]
